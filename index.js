f5e274483b9e461e7c3b69cbf63ae3620668054e
Stephen Mbau <mbaustephen@gmail.com>
2023-07-14 10:38:45 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function(task = 'go to the office') {
+    return `This Monday, I will ${task}.`;
+}
+function wrapAdjective (symbol = '*') {
+    const innerFunction = function (character = 'a hardworker') {
+        return `You are ${symbol}${character}${symbol}!`
+    }
+    return innerFunction;
+}
+console.log (wrapAdjective('*')('a hardworker'))
